ee7abdf872fb9b946396553e01df1d9c95789af9
BrotherTill <tilljeromekeb@gmail.com>
2023-08-13 20:39:44 +0200

    quick patch

--- src/Main/Java/tjirm/Tetris/Pieces/Blocks.java
+++ src/Main/Java/tjirm/Tetris/Pieces/Blocks.java
@@ -9,12 +9,12 @@ public class Blocks {
 
     public static Block[] fullLine;         //initialized in Tetris.Logic.Board
 
-    public static final Block GBi = new Block(true, new Color(96, 96, 96), "GBi", "GBfi", false).setType("I");
-    public static final Block GBo = new Block(true, new Color(96, 96, 96), "GBo", "GBo", false).setType("O");
-    public static final Block GBl = new Block(true, new Color(96, 96, 96), "GBl", "GBl", false).setType("L");
-    public static final Block GBj = new Block(true, new Color(96, 96, 96), "GBj", "GBj", false).setType("J");
-    public static final Block GBs = new Block(true, new Color(96, 96, 96), "GBs", "GBs", false).setType("S");
-    public static final Block GBz = new Block(true, new Color(96, 96, 96), "GBz", "GBz", false).setType("Z");
-    public static final Block GBt = new Block(true, new Color(96, 96, 96), "GBt", "GBt", false).setType("T");
+    public static final Block GBi = new Block(true, new Color(96, 96, 96), "GBi", "GBi", false);
+    public static final Block GBo = new Block(true, new Color(96, 96, 96), "GBo", "GBo", false);
+    public static final Block GBl = new Block(true, new Color(96, 96, 96), "GBl", "GBl", false);
+    public static final Block GBj = new Block(true, new Color(96, 96, 96), "GBj", "GBj", false);
+    public static final Block GBs = new Block(true, new Color(96, 96, 96), "GBs", "GBs", false);
+    public static final Block GBz = new Block(true, new Color(96, 96, 96), "GBz", "GBz", false);
+    public static final Block GBt = new Block(true, new Color(96, 96, 96), "GBt", "GBt", false);
 
 }
